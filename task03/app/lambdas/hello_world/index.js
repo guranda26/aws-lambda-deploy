1a4490280da3a4f4881b4518b757e88a25dc50db
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-07 20:49:36 +0400

    add integration type

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,17 +1,9 @@
 exports.handler = async (event) => {
-  try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Hello from Lambda",
-      }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "Internal server error",
-      }),
-    };
-  }
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from Lambda",
+    }),
+  };
+  return response;
 };
