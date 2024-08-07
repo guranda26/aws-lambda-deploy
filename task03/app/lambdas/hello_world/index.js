ee9e62240ec5fb64b02fdbe31ef8c987525f62b5
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-07 19:56:19 +0400

    change function response

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,9 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Lambda",
-    }),
+    message: "Hello from Lambda",
   };
   return response;
 };
