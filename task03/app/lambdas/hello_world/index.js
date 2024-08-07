2205a857b420cd4effeccff11ddd4fd7475f3b4f
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-08-07 22:50:18 +0400

    update index

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,24 +1,9 @@
 exports.handler = async (event) => {
-  console.log("Received event:", JSON.stringify(event, null, 2));
-
-  try {
-    // Process the event
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Hello from Lambda",
-      }),
-    };
-    console.log("Response:", JSON.stringify(response));
-    return response;
-  } catch (error) {
-    console.error("Error:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "Something went wrong",
-        error: error.message,
-      }),
-    };
-  }
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello from Lambda",
+    }),
+  };
+  return response;
 };
